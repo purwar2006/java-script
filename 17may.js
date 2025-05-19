f5db44589fe2632c29e8b7138e501dc6178c7dec
purwar2006 <abhishekpurwar2024@gmail.com>
2025-05-19 18:11:55 +0530

    for in/for of loop

---
+++ 17may.js
@@ -0,0 +1,36 @@
+// let ar=["ashu ",45," bpl"]
+
+// for(let a = 0;a < 4;a++){
+//     document.write(ar[a])
+// }
+
+
+// to find indexing value
+// for(let val in ar)
+// {
+//     document.write(val)
+// }
+
+
+
+
+
+
+
+// for of loop
+// for(let val of ar)
+// {
+//     document.write(val)
+// }
+
+
+
+
+    let ar = []
+    for (let a=0;a<4;a++)
+    {
+        ar[a] = parseInt(prompt("Enter the no"))
+    }
+    document.write(ar)
+
+
---
+++ 17task.js
@@ -0,0 +1,7 @@
+let num=parseInt(prompt("Enter the no "))
+document.write(num)
+
+let reverse=num%10
+document.write("reverse no- "(reverse))
+
+
