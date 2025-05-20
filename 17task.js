01f9a65688a6c3f436e9cdcf7b1b2ba3a2cf6586
purwar2006 <abhishekpurwar2024@gmail.com>
2025-05-20 16:44:49 +0530

    push or pop

--- 17task.js
+++ 17task.js
@@ -1,7 +1,35 @@
-let num=parseInt(prompt("Enter the no "))
-document.write(num)
+// let num=parseInt(prompt("Enter the no "))
+// document.write(num)
 
-let reverse=num%10
-document.write("reverse no- "(reverse))
+// let reverse=num%10
+// document.write("reverse no- "(reverse))
+
+
+
+// 10 inputs and sum of it
+// let num=[]
+// let sum=0
+// for (let r = 1; r <= 10; r++){
+//     num[r] = parseInt(prompt("Enter the no "))
+    
+//     sum=sum+num[r]
+// }
+
+// document.write(sum)
+
+//input from user then print last digit of a number
+let num=parseInt(prompt("Enter the no."))
+let=lastdigit=Math.abs(num % 10)
+document.write("the last digit of a number is " + lastdigit + "<br>")
+//input from user then print first digit of a number
+let firstdigit=num.toString()[0]
+document.write("the first digit of a number "+ firstdigit )
+document.write("<br>")
+//sum of both number
+sum = lastdigit + firstdigit
+document.write("the sum of both number is " + sum)
+
+
+
 
 
---
+++ 19.js
@@ -0,0 +1,38 @@
+// array pushing method
+// let ar=["abhishek",18,"bhopal",5.9]
+// console.log(ar)
+
+// ar.push("cybrom",9200)
+// console.log(ar)
+
+// ar.pop()
+// console.log(ar)
+
+// ar.unshift("my name"," is" )
+// console.log(ar)
+
+// ar.shift()
+// console.log(ar)
+//-------------------------------------------------------------------------------------//
+// slice use for breakout an small array from an array----------------------------------
+
+// let ar = ["abhishek", 18, "bhopal",5.9]
+// let a=ar.slice(1,3)
+// NOTE - always last indexing is (last-1)
+// example let a=ar.slice(1,2)
+// here ladst can run by 2-1=1
+// document.write(a)
+
+
+// let ar = ["abhishek", 18, "bhopal", 5.9]
+// ar.splice(1,3)
+// document.write(a)
+
+// ar.splice(1,0,"karan")
+// document.write(ar)
+
+
+
+
+
+
