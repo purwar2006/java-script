12cdfeec57d2e0cbe4e7f52be203cb8a135ac830
purwar2006 <abhishekpurwar2024@gmail.com>
2025-05-24 18:46:16 +0530

    onject types

---
+++ function24.js
@@ -0,0 +1,59 @@
+// types of functions------->
+// 1. with argumnet no return
+// 1. no argumnet no return
+// 1. with argumnet with return
+// 1. no argumnet with return
+
+
+// no return no argument---->
+// function fun() {
+//     console.log("i am no arg no return")
+// }
+// fun()
+
+
+// use to pass value in fuction from outside
+// function Value(a) {
+//     console.log(a);
+// }
+// Value("abhishek", 4)
+
+//task*********
+//second method
+// function Sum(a, b) {
+//     console.log(a + b)
+// }
+// Sum(
+//     parseInt(prompt("enter the value 1")),
+//     parseInt(prompt("enter the value 2"))
+// )
+
+
+// function Sum(a, b) {
+//     console.log(a + b)
+// }
+// let a = parseInt(prompt("enter the value 1"))
+// let b = parseInt(prompt("enter the value 2"))
+// Sum(a, b)
+
+
+
+//with arg and return-------------------------->
+//   function Demo(a){
+//     return a+a
+//   }
+//   Demo(10)
+//   console.log(Demo(10))
+
+  //** return value can be store in calling function
+
+//   function Demo(a){
+//     return a+a
+//   }
+//  let b= Demo(10)
+//   console.log(b)
+
+
+
+
+
